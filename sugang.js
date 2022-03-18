0a1a2516e25819a792debf0ec51dc32b63d2b11f
Sung Jae Hyuk <okaybody10@korea.ac.kr>
2022-03-18 18:21:38 +0900

    Add files via upload

---
+++ sugang.js
@@ -0,0 +1,63 @@
+let url = "https://sugang.korea.ac.kr/sugang?attribute=sugangMode&fake="
+let tm = null;
+let dt = new FormData();
+let cnt = 0, timer = 0;
+dt.set("mode", "insert");
+
+let classes =
+[
+    ['GEQR023','01'], 
+    ['MATH223','01'],
+    ['MATH321','00'],
+    ['HANM120','09']
+]
+
+function sugang(lists){
+    for(let i=0;i<lists.length;i++){
+    (function(i){
+            let cours = lists[i][0]+"@"+lists[i][1];
+            dt.set("params",cours);
+            $.ajax({
+                url : url + String(new Date().getTime()),
+                type : 'post',
+                async : false, 
+                processData: false,
+                data : (new URLSearchParams(dt)).toString(),
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+                dataType : 'json'
+            }).done(function(json){
+                console.log(cours);
+                console.log(json);
+                if(json.code=="200") cnt++;
+                console.log(cnt);	
+            });
+        })(i)
+    }
+}
+
+function res(lists){
+    tm=setTimeout(()=>{
+        sugang(lists);
+        if(cnt==lists.length || timer > 20) {
+            console.log("end!!!!"), tm=clearTimeout(tm);
+            return;
+        }
+        timer++;
+        res(lists);
+    }, 1000*3);
+    console.log(cnt);
+}
+
+//res(classes);
+
+let x = setInterval(()=>{
+    $('#menu_basket').click();
+    $('#menu_sugang').click();
+    console.log(new Date());
+    if((new Date())>(new Date(2022, 01, 17, 09, 59, 48))) {
+        res(classes);
+        clearInterval(x);
+    }
+}, 1000*2) // Set Data Please
+
+x;
